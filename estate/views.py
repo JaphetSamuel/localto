45e5f337f95c95bb6cd7def2b2413b58ddbf262e
JaphetSamuel <sagoua1@gmail.com>
2021-12-08 02:33:09 +0000

    modification du formulaire

--- estate/views.py
+++ estate/views.py
@@ -11,11 +11,14 @@ class SubmitPropretyView(CreateView):
     ModelForm = ProprieteForm
     fields = ['nom', 'adresse', 'ville', 'code_postal', 'superficie',
          'prix_vente','prix_location','enVente','enLocation', 'description']
-    template_name = 'submit-property.html'
+    template_name = 'estate/submit-property.html'
 
     def form_valid(self, form):
         form.instance.agent.user = self.request.user
         return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        return super().form_invalid(form)
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
